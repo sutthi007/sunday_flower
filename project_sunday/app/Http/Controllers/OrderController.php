591d5bf141fc71c4c0e29ff43ab0751c87d2f765
SUTTHI SAENJAI <89660259+sutthi007@users.noreply.github.com>
2022-03-25 16:57:36 +0700

    update code

---
+++ project_sunday/app/Http/Controllers/OrderController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\customer;
+use App\Models\Order;
+
+class OrderController extends Controller
+{
+   public function index(){
+
+       return view('Order.order');
+   }
+
+   public function store(Request $request){
+    $order = Order::create([
+        'name' => $request->name,
+        'city'=> $request->city,
+        'province'=> $request->province,
+        'type'=> $request->type,
+        'list'=> $request->list,
+        'quantity'=> $request->quantity,
+        'price'=> $request->price,
+        'customer_id' => $request->customer_id,
+    ]);
+    $customer = $request->customer_id;
+
+    return redirect()->route('FormOrder.show',$customer);
+}
+
+   public function show($test){
+        $customer = customer::find($test);
+        return view('Order.order-step-1', compact('customer'));
+    }
+
+    public function destroy($id){
+
+        $order = Order::find($id);
+        $order->delete();
+
+        return redirect()->route('projects.index');
+    }
+    public function update($id,Request $request){
+
+           $order =  Order::find($id);
+           $order->update($request->all());
+
+    }
+
+}
