a37adb4aa5e592902fa4af7ee6b90b3afeb9dc44
Neonartit <Neonartit@gmail.com>
2022-05-05 17:21:04 +0700

    more edit

--- project_sunday/resources/views/Order/bill.blade.php
+++ project_sunday/resources/views/Order/bill.blade.php
@@ -221,11 +221,14 @@
         </div>
         <div class= "w-full mt-10">
             <div class=" ">
-            <p class="text-xl ml-14">ผู้รับเงิน</p>
+            <p class="text-xl ml-14">ผู้รับเงิน</p> {{Auth::user()->name}}
             <p class="text-xl ml-15">(..............................................................)</p>
             </div>
             <div class="text-center mr-15 mt--20px">
-            <p class="text-xl float-right">วันที่.............../.............................../...................</p>
+                @php
+                    $mytime = Carbon\Carbon::now();
+                @endphp
+            <p class="text-xl float-right">วันที่  {{$mytime->format('Y/m/d')}}</p>
             </div>
         </div>
     </form>
