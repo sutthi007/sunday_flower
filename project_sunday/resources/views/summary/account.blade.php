bb77839373cd1356d388855d596714fac09a8751
SUTTHI SAENJAI <89660259+sutthi007@users.noreply.github.com>
2022-05-13 17:05:13 +0700

    add database to app

--- project_sunday/resources/views/summary/account.blade.php
+++ project_sunday/resources/views/summary/account.blade.php
@@ -687,17 +687,7 @@
                                 Lines
                             </h4>
                             <canvas id="line"></canvas>
-                            <div class="flex justify-center mt-4 space-x-3 text-sm text-gray-600 dark:text-gray-400">
-                                <!-- Chart legend -->
-                                <div class="flex items-center">
-                                    <span class="inline-block w-3 h-3 mr-1 bg-teal-500 rounded-full"></span>
-                                    <span>Organic</span>
-                                </div>
-                                <div class="flex items-center">
-                                    <span class="inline-block w-3 h-3 mr-1 bg-purple-600 rounded-full"></span>
-                                    <span>Paid</span>
-                                </div>
-                            </div>
+                           
                         </div>
                         <!-- Bars chart -->
                         <div class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
@@ -705,19 +695,7 @@
                                 Bars
                             </h4>
                             <canvas id="bars"></canvas>
-                            <div class="flex justify-center mt-4 space-x-3 text-sm text-gray-600 dark:text-gray-400">
-                                <!-- Chart legend -->
-                                <div class="flex items-center">
-                                    <span class="inline-block w-3 h-3 mr-1 bg-teal-500 rounded-full"></span>
-                                    <span>Shoes</span>
-                                </div>
-                                <div class="flex items-center">
-                                    <span class="inline-block w-3 h-3 mr-1 bg-purple-600 rounded-full"></span>
-                                    <span>Bags</span>
-                                </div>
-                            </div>
                         </div>
-                        
                     </div>
                 </div>
             </main>
